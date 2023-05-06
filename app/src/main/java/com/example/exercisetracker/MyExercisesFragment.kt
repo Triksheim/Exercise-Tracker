2f0188903f220d272a2abfa54d0b3ac43cd5dd97
AbigailSto <asc278@post.uit.no>
2023-05-06 03:15:45 +0200

    Exercise_item for recycler viw added. Adapter for recycleritem to be used in myExercisesFragment created and implemented. Fixed navigation between fragments

--- app/src/main/java/com/example/exercisetracker/MyExercisesFragment.kt
+++ app/src/main/java/com/example/exercisetracker/MyExercisesFragment.kt
@@ -1,10 +1,56 @@
 package com.example.exercisetracker
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
+import com.example.exercisetracker.adapters.ExerciseItemAdapter
 import com.example.exercisetracker.databinding.FragmentMyExercisesBinding
+import com.example.exercisetracker.repository.TrainingApplication
+import com.example.exercisetracker.viewmodel.SharedViewModel
+import com.example.exercisetracker.viewmodel.SharedViewModelFactory
+import kotlinx.coroutines.flow.collectLatest
 
 class MyExercisesFragment: Fragment() {
 
     private var _binding: FragmentMyExercisesBinding? = null
     private val binding get() = _binding!!
+
+    private val sharedViewModel: SharedViewModel by activityViewModels() {
+        SharedViewModelFactory(
+            (activity?.application as TrainingApplication).trainingRepository
+        )
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMyExercisesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ExerciseItemAdapter()
+
+        lifecycleScope.launchWhenStarted {
+            sharedViewModel.userExercises.collectLatest { userExercises -> adapter.submitList(userExercises) }
+        }
+
+        binding.apply {
+            exerciseRecycler.adapter = adapter
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
--- app/src/main/java/com/example/exercisetracker/adapters/ExerciseItemAdapter.kt
+++ app/src/main/java/com/example/exercisetracker/adapters/ExerciseItemAdapter.kt
@@ -1,29 +1,45 @@
 package com.example.exercisetracker.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.exercisetracker.MyExercisesFragmentDirections
 import com.example.exercisetracker.databinding.ExerciseItemBinding
 import com.example.exercisetracker.db.Exercise
+import androidx.navigation.fragment.findNavController
+import com.example.exercisetracker.db.UserExercise
 
-// Legg til- knappen for en øvelse i rececler view er ikke implementert
+// Legg til- knappen for en øvelse i recycler view er ikke implementert
+// En adapter for både MyExercises fragment (da vises edit knappen) og for ProgramDetailsFragment
+// (da vises legg til øvelse-knappen)
 class ExerciseItemAdapter()
-    : ListAdapter<Exercise, ExerciseItemAdapter.ExerciseViewHolder>(DiffCallback) {
+    : ListAdapter<UserExercise, ExerciseItemAdapter.ExerciseViewHolder>(DiffCallback) {
 
-    class ExerciseViewHolder(private var binding: ExerciseItemBinding): RecyclerView.ViewHolder(binding.root){
+    class ExerciseViewHolder(private var binding: ExerciseItemBinding)
+        : RecyclerView.ViewHolder(binding.root){
 
-        fun bind(userExercise: Exercise) {
+        fun bind(userExercise: UserExercise) {
             binding.exercise = userExercise
+            binding.buttonAdd.visibility= View.GONE
+            /** binding.buttonEdit.setOnClickListener {
+                val action = MyExercisesFragmentDirections
+                    .actionMyExercisesFragmentToNewExerciseFragment(userExercise.id)
+                findNavController().navigate(action)
+            } **/
             binding.executePendingBindings()
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
             ExerciseViewHolder {
-        return ExerciseViewHolder(ExerciseItemBinding.inflate(
-            LayoutInflater.from(parent.context)))
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ExerciseViewHolder(
+            ExerciseItemBinding.inflate(layoutInflater, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: ExerciseViewHolder, position: Int) {
@@ -31,12 +47,12 @@ class ExerciseItemAdapter()
         holder.bind(userExercise)
     }
 
-    companion object DiffCallback: DiffUtil.ItemCallback<Exercise>() {
-        override fun areItemsTheSame(oldItem: Exercise, newItem: Exercise): Boolean {
+    companion object DiffCallback: DiffUtil.ItemCallback<UserExercise>() {
+        override fun areItemsTheSame(oldItem: UserExercise, newItem: UserExercise): Boolean {
             return oldItem.id == newItem.id
         }
 
-        override fun areContentsTheSame(oldItem: Exercise, newItem: Exercise): Boolean {
+        override fun areContentsTheSame(oldItem: UserExercise, newItem: UserExercise): Boolean {
             return oldItem.description == newItem.description
         }
     }
