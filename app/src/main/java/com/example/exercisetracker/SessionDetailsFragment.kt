320d41e14debc721d71b9f7bec1e375ceb68c8c6
AbigailSto <asc278@post.uit.no>
2023-05-27 17:21:49 +0200

    No changes in code. Added desciptive comments

--- app/src/main/java/com/example/exercisetracker/SessionDetailsFragment.kt
+++ app/src/main/java/com/example/exercisetracker/SessionDetailsFragment.kt
@@ -52,6 +52,7 @@ class SessionDetailsFragment : Fragment(), OnMapReadyCallback {
 
         sharedViewModel.setToolbarTitle(getString(R.string.title_session_details))
 
+        // Observe currentDiaplayableSession, bind viewmodel and views
         sharedViewModel.currentDisplayableSession.observe(this.viewLifecycleOwner) { displayableSession ->
             binding.apply {
                 lifecycleOwner = viewLifecycleOwner
@@ -63,6 +64,7 @@ class SessionDetailsFragment : Fragment(), OnMapReadyCallback {
                 )
                 sessionIcon.setImageResource(resourceId)
 
+                // Set visibility of views displaying session data based upon user choices
                 when (displayableSession.useTiming) {
                     0 -> cardTime.visibility = View.GONE
                     1 -> cardTime.visibility = View.VISIBLE
@@ -115,10 +117,13 @@ class SessionDetailsFragment : Fragment(), OnMapReadyCallback {
 
                 // Fetch exercises for the current program:
                 sharedViewModel.flowExercisesForCurrentProgram()
+
                 // Observe the LiveData of program exercises
                 sharedViewModel.userProgramExercises.observe(viewLifecycleOwner, Observer { programExercises ->
+
                     // Fetch the list of all user exercises
                     val userExercises = sharedViewModel.userExercises.value
+
                     // Filter the user exercises that are in the program
                     val exercisesForProgram = userExercises.filter { userExercise ->
                         programExercises.any { programExercise ->
@@ -132,6 +137,7 @@ class SessionDetailsFragment : Fragment(), OnMapReadyCallback {
         }
     }
 
+    // Initialize interface for buttons on the exercise-items in recyclerView
     private fun getExerciseClickListener(): ExerciseItemAdapter.ExerciseClickListener {
         return object : ExerciseItemAdapter.ExerciseClickListener {
             override fun onEditButtonClick(userExercise: UserExercise) {} // Not Visible in this fragment
